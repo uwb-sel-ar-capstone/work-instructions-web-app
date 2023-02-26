03412e033bca032107827f34e26b569422ab9ea0
Nic Bestauros <nbestauros@gmail.com>
2023-02-26 13:03:00 -0800

    Added additional endpoint for drop db.

--- work-instructions-backend/routes/debug.js
+++ work-instructions-backend/routes/debug.js
@@ -4,5 +4,6 @@ const router = express.Router();
 const { populateDB, deleteDB } = require("../controllers/debug");
 
 router.route("/").get(populateDB).delete(deleteDB);
+router.route("/delete").get(deleteDB);
 
 module.exports = router;
