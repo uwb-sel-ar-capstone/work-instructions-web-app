a9c2a3dfcc65019ae83b60b3452ae053a71e7f95
Zeki-M <zekimohammed3837@gmail.com>
2023-02-27 16:57:08 -0800

    Added Connection to API

--- work-instructions-frontend/src/components/StepCard.jsx
+++ work-instructions-frontend/src/components/StepCard.jsx
@@ -2,38 +2,39 @@ import Card from 'react-bootstrap/Card';
 import ListGroup from 'react-bootstrap/ListGroup';
 import Button from 'react-bootstrap/Button';
 
+const StepCard = ({ step }) => {
+  const { _id, text, item, positions } = step || {};
 
-const StepCard = ({ workInstructionID, positions }) => {
   return (
-    <>
-      <div style = {{margin : "auto" , display :"flex", 'justify-content' : "center"  }}> 
-
-      <Card style={{ width: '18rem' }}>
-      <Card.Body>
-        <Card.Title style = {{color : "black", "textAlign" : "center"}}
-        >Card Title, "Step Name"
-
-        </Card.Title>
-        <Card.Text style = {{color : "black", "textAlign" : "center"}}>
-          Work Instruction Step Text
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
-        </Card.Text>
-      </Card.Body>
-      <ListGroup className="list-group-flush">
-        <ListGroup.Item>postions1</ListGroup.Item>
-        <ListGroup.Item>postions2</ListGroup.Item>
-        <ListGroup.Item>postions3</ListGroup.Item>
-      </ListGroup>
-      <Card.Body style = {{margin : "auto" , display :"flex", 'justify-content' : "center"  }}>
-      <Button variant="outline-primary">Add New Postion</Button>{' '}
-      </Card.Body>
-    </Card>
-
-      </div>
-    </>
+    <div style={{ margin: 'auto', display: 'flex', justifyContent: 'center' }}>
+      <Card style={{ width: '30rem' }}>
+        <Card.Body>
+          <Card.Title style={{ color: 'black', textAlign: 'center' }}>
+            Step ID: {_id || 'Step Name'}
+            <br />
+            Step Text: {text || 'Step Name'}
+          </Card.Title>
+          <Card.Text style={{ color: 'black', textAlign: 'center' }}>
+            Componet: {item ? item.name : 'Work Instruction Step Text'}
+          </Card.Text>
+        </Card.Body>
+        
+        <ListGroup className='list-group-flush'>
+          {positions && positions.map((position, index) => (
+            <ListGroup.Item key={index}>
+              Start postion: ({position.xStart}, {position.zStart})
+              <br/>
+              End postion: ({position.xEnd}, {position.zEnd})
+            </ListGroup.Item>
+          ))}
+        </ListGroup>
+        <Card.Body style={{ margin: 'auto', display: 'flex', justifyContent: 'center' }}>
+          <Button variant='outline-primary'>Add New Position</Button>
+        </Card.Body>
+      </Card>
+    </div>
   );
 };
-export default StepCard;
 
+export default StepCard;
 
--- work-instructions-frontend/src/components/StepList.jsx
+++ work-instructions-frontend/src/components/StepList.jsx
@@ -1,10 +1,43 @@
 import StepCard from "./StepCard";
+import { useState, useEffect } from 'react';
+
 const StepList = ({ workInstructionID }) => {
+  const [steps, setSteps] = useState([]);
+  const [currentStep, setCurrentStep] = useState(0);
+
+  useEffect(() => {
+    fetch(`http://localhost:5000/api/v1/steps?workInstructionID=${workInstructionID}`)
+      .then(res => res.json())
+      .then(data => {
+        console.log(data);
+        setSteps(data.steps);
+      })
+      .catch(err => console.log(err));
+  }, [workInstructionID]);
+
+  const handleNextStep = () => {
+    setCurrentStep(currentStep + 1);
+  }
+
+  const handlePreviousStep = () => {
+    setCurrentStep(currentStep - 1);
+  }
+
+  const step = steps[currentStep] || {};
+
   return (
     <>
-      <h1>Hello World</h1>
-      <StepCard workInstructionID={workInstructionID} />
+      <StepCard step={step} />
+      <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+        {currentStep > 0 && (
+          <button onClick={handlePreviousStep}>Previous Step</button>
+        )}
+        {currentStep < steps.length - 1 && (
+          <button onClick={handleNextStep}>Next Step</button>
+        )}
+      </div>
     </>
   );
 };
+
 export default StepList;
