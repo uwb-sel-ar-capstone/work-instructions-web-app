cf9880b76979318d9a473cde39968a7945a44f50
YounesLahrichi <Lahrichi.ys@gmail.com>
2023-03-02 19:53:34 -0800

    Updated Environment ETC

--- work-instructions-frontend/src/components/StepList.jsx
+++ work-instructions-frontend/src/components/StepList.jsx
@@ -1,10 +1,45 @@
 import StepCard from "./StepCard";
-const StepList = ({ workInstructionID }) => {
+import "./styles.css"
+
+import Accordion from '@mui/material/Accordion';
+import AccordionSummary from '@mui/material/AccordionSummary';
+import AccordionDetails from '@mui/material/AccordionDetails';
+import Typography from '@mui/material/Typography';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+
+
+const StepList = ({ workInstruction }) => {
+
   return (
-    <>
-      <h1>Hello World</h1>
-      <StepCard workInstructionID={workInstructionID} />
-    </>
+    <div className="stepList">
+      <h5 className="heading">Title: <b>Step List</b></h5>
+      <div className="stepDisplayBox">
+        {workInstruction.steps.map((item, index) => {
+          return  <div className = "buttonStyle">
+            <Accordion>
+              <AccordionSummary
+                aria-controls="panel1a-content"
+                id="panel1a-header"
+                expandIcon={<ExpandMoreIcon/>}
+              >
+                <Typography>{item.text}</Typography>
+              </AccordionSummary>
+              <AccordionDetails>
+                <Typography>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+                  malesuada lacus ex, sit amet blandit leo lobortis eget.
+                </Typography>
+              </AccordionDetails>
+            </Accordion>
+          </div>
+        })}
+      </div>
+      <StepCard workInstruction={workInstruction} />
+    </div>
   );
+
 };
+
 export default StepList;
+
+{/*<Button className='button' variant="contained">{item.text}</Button> */}
