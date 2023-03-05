03a9201531a2a920304ea9dc6b187dac2753118c
Nic Bestauros <nbestauros@gmail.com>
2023-03-04 17:19:15 -0800

    Added bootstrap-override wrapper to entire app.

--- work-instructions-frontend/src/index.js
+++ work-instructions-frontend/src/index.js
@@ -1,16 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { AppProvider } from "./context";
 import "bootstrap/dist/css/bootstrap.min.css";
+import "./index.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <AppProvider>
-      <App />
+      <div id="bootstrap-override">
+        <App />
+      </div>
     </AppProvider>
   </React.StrictMode>
 );
