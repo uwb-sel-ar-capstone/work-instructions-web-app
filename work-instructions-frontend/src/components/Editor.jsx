da162a572d69914123ea86b6d92c677aaf658df5
Nic Bestauros <nbestauros@gmail.com>
2023-04-10 16:18:55 -0700

    Added Save Button.

--- work-instructions-frontend/src/components/Editor.jsx
+++ work-instructions-frontend/src/components/Editor.jsx
@@ -4,16 +4,34 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 import Image from "./Image";
 import { useGlobalContext } from "../context";
-import PositionSelector from "./PositionSelector";
+import Button from "react-bootstrap/Button";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
 const Editor = () => {
   const { baseAPIUrl } = useGlobalContext();
   // eslint-disable-next-line
   const [searchParams, setSearchParams] = useSearchParams();
   const workInstructionID = searchParams.get("workInstructionID") || "";
-  const [workInstruction, setWorkInstruction] = useState({});
+  const [workInstruction, setWorkInstruction] = useState({
+    dimensions: {
+      xLengthCM: 0,
+      zLengthCM: 0,
+    },
+    _id: "",
+    name: "",
+    steps: [],
+    image: {
+      _id: "",
+      imageData: "",
+      mimeType: "",
+      encoding: "",
+    },
+  });
   const [imageID, setImageID] = useState("");
-  const [image, setImage] = useState({});
+  const [image, setImage] = useState({}); // image is stored here as this is a large file and we don't want to repeatedly call for in child components
+  const [isCreateNew, setIsCreateNew] = useState(workInstructionID === ""); // If there is no id, then this is a new work instruction
 
   const getWorkInstruction = async (url) => {
     try {
@@ -32,23 +50,57 @@ const Editor = () => {
     }
   };
   useEffect(() => {
+    if (isCreateNew) {
+      return;
+    }
     getWorkInstruction(
-      `${baseAPIUrl}/workinstructions/${workInstructionID}?imageData=true`
+      `${baseAPIUrl}/workinstructions/${workInstructionID}?imageData=true&populate=false`
     );
   }, [baseAPIUrl, workInstructionID]);
 
+  const checkValidWorkInstruction = () => {
+    console.log(workInstruction);
+    if (workInstruction.dimensions.xLengthCM === 0) {
+      return false;
+    }
+    if (workInstruction.dimensions.zLengthCM === 0) {
+      return false;
+    }
+    if (workInstruction.name === "") {
+      return false;
+    }
+    if (workInstruction.steps.length === 0) {
+      return false;
+    }
+    if (workInstruction.image._id === "") {
+      return false;
+    }
+    return true;
+  };
+
   return (
     <>
-      <h1>Hello World</h1>
-      <StepList workInstructionID={workInstructionID} />
-      <Image
-        workInstruction={workInstruction}
-        workInstructionID={workInstructionID}
-        image={image}
-        imageID={imageID}
-        setImage={setImage}
-        setImageID={setImageID}
-      />
+      <Container gap={3} classname="col-md-5 mx-auto">
+        <Row className="justify-content-md-center">
+          <Col md="auto">
+            <Image
+              workInstruction={workInstruction}
+              workInstructionID={workInstructionID}
+              image={image}
+              imageID={imageID}
+              setImage={setImage}
+              setImageID={setImageID}
+            />
+          </Col>
+        </Row>
+        <Row className="justify-content-md-center">
+          <Col xs="auto" md="auto">
+            <Button variant="primary" disabled={!checkValidWorkInstruction()}>
+              Save
+            </Button>
+          </Col>
+        </Row>
+      </Container>
     </>
   );
 };
