ebb38f466bd65409527565bbbeca9cd240ef3565
Nic Bestauros <nbestauros@gmail.com>
2023-06-04 20:22:00 -0700

    Removed basic frontend.

--- basic-frontend/scripts.js
+++
@@ -1,6 +0,0 @@
-const doSomething = () => {
-  let exampleInput = document.getElementById("exampleInput").value;
-  document.getElementById("exampleParagraph").textContent = exampleInput;
-};
-
-document.getElementById("exampleButton").addEventListener("click", doSomething);
